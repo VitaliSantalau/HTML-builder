1e022e61bb5849a5c4fea8cd9130ff9bb473a88a
VitaliSantalau <v.santalau@gmail.com>
2021-10-24 19:25:18 +0300

    feat: add solution 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+const stdout = require('process').stdout;
+
+const absolutPath = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(absolutPath, 'utf8');
+
+readStream.pipe(stdout); // or take a look below 
+
+// *** OR *** 
+
+// readStream.on('data', function(chunk) {
+//   console.log(chunk);
+// });
