eb899c5ed71c99bfcc4c19d4d0eff0a06a472d7d
VitaliSantalau <v.santalau@gmail.com>
2021-11-03 01:07:06 +0300

    refactor: refactor solution 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -7,15 +7,18 @@ const { stdin: input, stdout: output } = process;
 
 const rl = readline.createInterface({ input, output });
 
-const absolutPath = path.join(__dirname, 'text.txt');
-const writeStream = fs.createWriteStream(absolutPath, 'utf8');
+const pathToOutFile = path.join(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(pathToOutFile, 'utf8');
 
 console.log('Hi, type something...');
 
 rl.on('line', (input) => {
   if(input === 'exit') {
     rl.close();
-  } else writeStream.write(input);
+  } else {
+    writeStream.write(input);
+    writeStream.write('\n');
+  }
 });
 
 process.on('beforeExit', () => {
